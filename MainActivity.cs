f0e149a4c39b74c690a13d66f553ccc54befdb78
FabCamposs <74834792+FabCamposs@users.noreply.github.com>
2021-06-15 10:41:20 -0500

    Add files via upload

---
+++ MainActivity.cs
@@ -0,0 +1,87 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System.Xml.Serialization;
+using System.IO;
+using System.Data;
+using Android.Content;
+
+namespace AplicacionTienda
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+            
+           
+            var btnGuardar = FindViewById<Button>(Resource.Id.btnguardar);
+            var btnConsult = FindViewById<Button>(Resource.Id.btnconsultar);
+            var txtNombre = FindViewById<EditText>(Resource.Id.txtnombre);
+            var txtDomicilio = FindViewById<EditText>(Resource.Id.txtdomicilio);
+            var txtCorreo = FindViewById<EditText>(Resource.Id.txtcorreo);
+            var txtFecha = FindViewById<EditText>(Resource.Id.txtdia);
+            var txtHora = FindViewById<EditText>(Resource.Id.txthora);
+            var imagen = FindViewById<ImageView>(Resource.Id.image);
+            imagen.SetImageResource(Resource.Drawable.logo);
+
+
+            btnConsult.Click += delegate
+            {
+                try
+                {
+                    Cargar();
+                }
+               
+                catch (System.Exception ex)
+                {
+                    Toast.MakeText(this, ex.Message, ToastLength.Long).Show();
+                }
+            };
+
+            btnGuardar.Click += delegate
+            {
+                try
+                {
+                    var WS = new ServicioWebSalud.ServicioWebSalud();
+                    if (WS.Guardar(txtNombre.Text, txtDomicilio.Text, txtCorreo.Text, txtFecha.Text, int.Parse(txtHora.Text)))
+                        Toast.MakeText(this, "Guardado correctamente en SQL Server", ToastLength.Long).Show();
+                    else
+                    {
+                        Toast.MakeText(this, "No guardado", ToastLength.Long).Show();
+                    }
+                }
+                catch (System.Exception ex)
+                {
+                    Toast.MakeText(this, ex.Message, ToastLength.Long).Show();
+                }
+            };
+
+        }
+   
+
+        public void Cargar()
+        {
+            var VistaPrincipal = new Intent(this, typeof(Principal));
+            StartActivity(VistaPrincipal);
+        }
+        public class Datos
+        {
+            public string Nombre;
+            public string Domicilio;
+            public string Correo;
+            public string Fecha;
+            public string Hora;
+
+
+        }
+
+
+
+
+    }
+}
---
+++ Principal.cs
@@ -0,0 +1,58 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+
+namespace AplicacionTienda
+{
+    [Activity(Label = "Principal")]
+    class Principal : Activity
+    {
+    
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.VistaPrincipal);
+            var btnBuscar = FindViewById<Button>(Resource.Id.btnbuscar); 
+            var txtNombre = FindViewById<EditText>(Resource.Id.txtnombre);
+            var txtDomicilio = FindViewById<EditText>(Resource.Id.txtdomicilio);
+            var txtCorreo = FindViewById<EditText>(Resource.Id.txtcorreo);
+            var txtFecha = FindViewById<EditText>(Resource.Id.txtdia);
+            var txtHora = FindViewById<EditText>(Resource.Id.txthora);
+            var imagen = FindViewById<ImageView>(Resource.Id.image);
+            imagen.SetImageResource(Resource.Drawable.logo);
+
+
+            btnBuscar.Click += delegate
+            {
+                var Conjunto = new DataSet();
+                DataRow Renglon;
+                try
+                {
+                    var WS = new ServicioWebSalud.ServicioWebSalud();
+                    Conjunto = WS.Busqueda((txtNombre.Text));
+                    Renglon = Conjunto.Tables["Datos"].Rows[0];
+                    txtDomicilio.Text = Renglon["Domicilio"].ToString();
+                    txtCorreo.Text = Renglon["Correo"].ToString();
+                    txtFecha.Text = Renglon["Fecha"].ToString();
+                    txtHora.Text = Renglon["Hora"].ToString();
+
+                }
+                catch (System.Exception ex)
+                {
+                    Toast.MakeText(this, ex.Message, ToastLength.Long).Show();
+                }
+
+            }; 
+
+        }
+    }
+}
